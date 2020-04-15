9a6a440afafd73049924a838b7288f0710163020
Devamakesh <47493737+devamakeshr@users.noreply.github.com>
2020-04-15 23:02:37 +0530

    a game is create

--- pygame-git/main.py
+++ pygame-git/main.py
@@ -11,9 +11,18 @@ pygame.init()
 screen = pygame.display.set_mode((800, 500))
 #title and icon
 
-pygame.display.set_caption("Coronavirus")
-icon = pygame.image.load("D:\Python\pygame\icom.png")
+pygame.display.set_caption("Star War")
+icon = pygame.image.load(r"D:\Python\pygame\pygame-git\villain (1).png")
 pygame.display.set_icon(icon)
+player_image_path = r"D:\Python\pygame\pygame-git\space-invaders.png"
+player_image = pygame.image.load(player_image_path)
+player_x = 350
+player_y = 400
+
+
+def player():
+    screen.blit(player_image, (player_x, player_y))
+
 
 # game loop
 value = True
@@ -22,4 +31,5 @@ while value:
         if event.type == pygame.QUIT:
             value = False
     screen.fill((255, 223, 0))
+    player()
     pygame.display.update()
