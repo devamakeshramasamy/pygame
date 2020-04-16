9926b12030d83e0af5941ee2db242b0c704323b5
Devamakesh <47493737+devamakeshr@users.noreply.github.com>
2020-04-16 10:54:06 +0530

    python keyboad access

--- pygame-git/main.py
+++ pygame-git/main.py
@@ -18,18 +18,45 @@ player_image_path = r"D:\Python\pygame\pygame-git\space-invaders.png"
 player_image = pygame.image.load(player_image_path)
 player_x = 350
 player_y = 500
+player_x_change = 0
+enemy_image_path = r"D:\Python\pygame\pygame-git\cartoon.png"
+enemy_image = pygame.image.load(enemy_image_path)
+enemy_x = 350
+enemy_y = 50
 
 
-def player():
-    screen.blit(player_image, (player_x, player_y))
+def player(x, y):
+    screen.blit(player_image, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemy_image, (x, y))
 
 
 # game loop
 value = True
 while value:
+    screen.fill((255, 223, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             value = False
-    screen.fill((255, 223, 0))
-    player()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_x_change = -0.3
+            elif event.key == pygame.K_RIGHT:
+                player_x_change = +0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_x_change = 0
+            print("key is release")
+
+    player_x += player_x_change
+    if player_x < 0:
+        player_x = 0
+    elif player_x > 736:
+        player_x = 736
+    player(player_x, player_y)
+    enemy(enemy_x, enemy_y)
+
     pygame.display.update()
